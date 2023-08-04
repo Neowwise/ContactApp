9b9b8e8908a4b3f3a2dd9d0ca6ff17954763753d
Boyukxanım İbrahimova <biglady004@gmail.com>
2023-08-04 13:09:03 +0400

    ContactApptask

---
+++ Taskk4/src/ContactApp/Contact.java
@@ -0,0 +1,36 @@
+package ContactApp;
+public class Contact extends ContactManager{
+	private String name;
+	private String surname;
+	private String phonenumber;
+	private String email;	
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	
+
+}
---
+++ Taskk4/src/ContactApp/Interface/package-info.java
@@ -0,0 +1,1 @@
+package ContactApp.Interface;
