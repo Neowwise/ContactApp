db1466b8b9e02dbdd61f62f5d0039a0de3dfda2d
Boyukxanım İbrahimova <biglady004@gmail.com>
2023-08-11 15:45:29 +0400

    ContactApp

---
+++ Taskk4/src/ContactApp/ContactManager.java
@@ -0,0 +1,52 @@
+package ContactApp;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+import ContactApp.Interface.ContactService;
+
+public class ContactManager implements ContactService{
+public String ffayladresi = "C:\\Users\\USER\\Desktop\\datalar.txt";
+
+		
+	@Override
+	public void Add() {
+		Scanner sc = new Scanner(System.in);
+		    Contact c = new Contact();	
+		    System.out.println("Adiniz");
+			c.setName(sc.next());
+			System.out.println("Soyadiniz");
+			c.setSurname(sc.next());
+			System.out.println("Nomreniz");
+			c.setPhonenumber(sc.next());
+			System.out.println("Emailniz");
+			c.setEmail(sc.next());
+			System.out.println("Yeni kontakt:" + c.getName() + " " + c.getSurname() + " " + c.getPhonenumber() +  " " + c.getEmail() + "\n");
+			try {
+				BufferedWriter bw = new BufferedWriter(new FileWriter(ffayladresi,true));
+				bw.write(c.getName() + " " + c.getSurname() + " " + c.getPhonenumber() +  " " + c.getEmail() + "\n") ;
+				bw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
+	}
+	@Override
+	public void getAll() {
+		try {
+			BufferedReader br  = new BufferedReader(new FileReader(ffayladresi));
+			String text;
+			while((text = br.readLine()) != null) {
+				System.out.println(text);
+			}
+			br.close();
+		}  catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+}
---
+++ Taskk4/src/ContactApp/Interface/ContactService.java
@@ -0,0 +1,12 @@
+package ContactApp.Interface;
+
+public interface ContactService {
+	
+
+	public void Add();
+	
+	public void getAll();
+	
+
+	
+}
